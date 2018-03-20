d14b5f7cb4bf6d8a6ddb22ef993c7b2204f553ff
Nathan Jangula <njangula@myriadmobile.com>
2018-03-20 09:58:56 -0500

    Add a swizzler for plugins

---
+++ Droar/Classes/Extensions/Swizzler.swift
@@ -0,0 +1,21 @@
+//
+//  Swizzling.swift
+//  Pods
+//
+//  Created by Nathan Jangula on 8/15/17.
+//
+//
+
+import Foundation
+
+public class Swizzler {
+    static func swizzleInstanceSelector(instance: NSObject, origSelector: Selector, newSelector: Selector) {
+        let aClass: AnyClass = object_getClass(instance)!
+        
+        let origMethod = class_getInstanceMethod(aClass, origSelector)!
+        let newMethod = class_getInstanceMethod(aClass, newSelector)!
+        
+        method_exchangeImplementations(origMethod, newMethod)
+    }
+}
+
