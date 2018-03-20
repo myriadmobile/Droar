4ece922973ab50102f2aad53c704b88d850d3a03
Nathan Jangula <njangula@myriadmobile.com>
2018-03-20 10:19:56 -0500

    Expose Swizzler properly

--- Droar/Classes/Extensions/Swizzler.swift
+++ Droar/Classes/Extensions/Swizzler.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public class Swizzler {
-    static func swizzleInstanceSelector(instance: NSObject, origSelector: Selector, newSelector: Selector) {
+    public static func swizzleInstanceSelector(instance: NSObject, origSelector: Selector, newSelector: Selector) {
         let aClass: AnyClass = object_getClass(instance)!
         
         let origMethod = class_getInstanceMethod(aClass, origSelector)!
